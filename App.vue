c9e29a5628199a2dae0f3c9561b2ed0fce48b42d
nastja123456789 <60920907+nastja123456789@users.noreply.github.com>
2021-04-05 22:39:44 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <the-header />
+
+    <div class="wrapper">
+      <router-view />
+    </div>
+
+    <burger-menu />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/layout/TheHeader'
+import BurgerMenu from '@/components/shared/BurgerMenu'
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader,
+    BurgerMenu
+  }
+}
+</script>
